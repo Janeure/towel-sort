797bfa133516c0ed0fff67cf7bd17f6f81a9e2e4
Евгения Рогозова <lalwat@mail.ru>
2022-07-14 03:21:33 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(matrix === undefined) {
+        return [];
+      }
+    var nameLengths = matrix.map(function(item, index) {
+        if(index === 0 || index % 2 === 0){
+          return item;
+        } else if(index % 2 !== 0 ) {
+          return item.reverse();
+        }
+      });
+        return nameLengths.flat();
 }
